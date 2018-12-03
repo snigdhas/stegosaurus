180b1b1c17c51b7f363cca1b41530ba49d585221
Snigdha Sharma <snigdhanoor@gmail.com>
2018-12-03 15:50:31 -0500

    Update decoder

--- decoderFiles/randomizedDecoder.py
+++ decoderFiles/randomizedDecoder.py
@@ -7,6 +7,7 @@ import json
 import random
 import string
 import sys
+import pprint
 
 
 REDUNDANCY = 8
@@ -33,11 +34,34 @@ def encode(user_input):
 
     print(json.dumps(input_to_patterns))
     
-def decode(encoded_text):
-    encoded_characters = []
-    for pattern in json.loads(encoded_text):
-        a = [tuple(x.split()) for x in pattern]
-        encoded_characters.append((tuple(a)))
+def column(matrix, i):
+    return [row[i] for row in matrix]
+
+def decode(message):
+    encodedMessage = []
+    decodedMessage = set()
+    for msg in message:
+        new_message = []
+        for i in range(8):
+            new_message.append(tuple(map(lambda x: tuple(x.split(" ")), msg[i * 3 : i * 3 + 3])))
+        encodedMessage.append(tuple(new_message))
+    # pprint.pprint(encodedMessage)
+    for i in range(8):
+        msg = column(encodedMessage, i)
+        # if i == 0: pprint.pprint(msg)
+        decodedMessage.add(decode_message(msg))
+    if len(decodedMessage) == 1:
+        return decodedMessage.pop()
+    else:
+        pass
+
+def decode_message(encoded_characters):
+    # encoded_characters = []
+    # for pattern in json.loads(encoded_text):
+    #     a = [tuple(x.split()) for x in pattern]
+    #     encoded_characters.append((tuple(a)))
+
+    # pprint.pprint(encoded_characters)
         
     key = encoded_characters[0]
     mapping = gen_character_mapping(key)
@@ -49,7 +73,9 @@ def decode(encoded_text):
         encoded_characters = encoded_characters[::-1]
     mapping = dict(map(reversed, mapping.items()))
 
-    print(''.join(mapping[char] for char in encoded_characters[1:-1]))
+    decodedString = ''.join(mapping[char] for char in encoded_characters[1:-1])
+    # print(decodedString)
+    return decodedString
 
 
 def main():
